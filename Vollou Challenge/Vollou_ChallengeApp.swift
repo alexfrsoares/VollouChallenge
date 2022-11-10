935c7594361a99dba380f07bf50aa9aa571a1344
Alex <otimizalex@outlook.com>
2022-11-10 01:02:11 +0000

    Xcode project created

---
+++ Vollou Challenge/Vollou_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vollou_ChallengeApp.swift
+//  Vollou Challenge
+//
+//  Created by Alex Freitas on 10/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct Vollou_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
